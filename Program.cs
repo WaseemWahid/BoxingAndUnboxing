433e549cce8d57cb9b410570c442c7640322f1b7
Waseem Wahid <wahid.waseem@gmail.com>
2022-02-07 17:47:04 -0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BoxingAndUnboxing
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<object> mixedList = new List<object>();
+            mixedList.Add(7);
+            mixedList.Add(28);
+            mixedList.Add(-1);
+            mixedList.Add(true);
+            mixedList.Add("chair");
+            int objSum = 0;
+            foreach (object item in mixedList)
+            {
+                if (item is int)
+                {
+                    objSum += (int)item;
+                }
+                Console.WriteLine(item);
+            }
+            Console.WriteLine(objSum);
+        }
+    }
+}
